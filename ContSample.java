9f3747cb632a33a002525bb4ff7bf826b264e284
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2022-04-15 21:48:28 +0900

    Add functions equivalent to GO_TO, PERFORM and PERFORM THRU of COBOL.

--- ContSample.java
+++ ContSample.java
@@ -1,5 +1,6 @@
 import java.util.Optional;
 
+
 public class ContSample {
 	public static void main(String[] args) {
 		CobolProgram program = new CobolProgram();
@@ -10,72 +11,40 @@ public class ContSample {
 	}
 }
 
+class MyException extends Exception {}
+
 class CobolProgram {
-	private int count = 0;
-	private Cont StartLabel = new Cont() {
-		public Optional<Cont> run() throws MyException {
-			System.out.println("Start");
-			return Optional.of(LoopLabel);
-		}
-	};
-	
-	private Cont LoopLabel = new Cont() {
-		public Optional<Cont> run() throws MyException {
-			System.out.println("count = " + count);
-			if(count >= 5) {
-				return Optional.of(EndLabel);
-			}
-			count++;
-			return Optional.of(LoopLabel);
-		}
-	};
-	
-	private Cont EndLabel = new Cont() {
-		public Optional<Cont> run() throws MyException {
-			System.out.println("End");
-			return Optional.of(Cont.pure());
-		}
-	};
-	
-	public void runProgram() throws MyException {
-		Optional<Cont> nextLabel = Optional.of(StartLabel);
-		while(nextLabel.isPresent()) {
-			Cont program = nextLabel.get();
-			nextLabel = program.run();
-		}
-	}
-}
-/*
-class CobolProgram2 {
-	private int count = 0;
 	private Cont[] contList = {
-		new Cont() {
+		new Cont(0) {
 			public Optional<Cont> run() throws MyException {
 				System.out.println("Start");
-				return Optional.of(contList[1]);
+				Cont.performThrough(contList, 1, 2).run();
+				Cont.perform(contList, 3).run();
+				return Cont.goTo(contList[2]).run();
 			}
 		},
 	
-		new Cont() {
+		new Cont(1) {
 			public Optional<Cont> run() throws MyException {
-				System.out.println("count = " + count);
-				if(count >= 5) {
-					return Optional.of(contList[2]);
-				}
-				count++;
-				return Optional.of(contList[1]);
+				System.out.println("Perform Through 1");
+				return Optional.of(contList[2]);
 			}
 		},
-			
-		new Cont() {
+
+		new Cont(2) {
 			public Optional<Cont> run() throws MyException {
-				System.out.println("End");
-				Cont.performThrough(contList, 3, 5);
-				throw new MyException();
-				//return Optional.of(contList[3]);
+				System.out.println("Perform Through 2");
+				return Optional.of(contList[3]);
 			}
 		},
-		
+
+		new Cont(3) {
+			public Optional<Cont> run() throws MyException {
+				System.out.println("Perform 3");
+				return Optional.of(contList[4]);
+			}
+		},
+
 		Cont.pure()
 	};
 	
@@ -86,14 +55,18 @@ class CobolProgram2 {
 			nextLabel = program.run();
 		}
 	}
-}*/
-
-class MyException extends Exception {
 }
 
 abstract class Cont {
 	abstract public Optional<Cont> run() throws MyException;
 	public int contId = -1;
+	public Cont() {
+		this.contId = -1;
+	}
+	public Cont(int contId) {
+		this.contId = contId;
+	}
+
 	static Cont pure() {
 		return new Cont() {
 			public Optional<Cont> run() throws MyException {
@@ -102,7 +75,7 @@ abstract class Cont {
 		};
 	}
 	
-	static Cont go_to(Cont cont) {
+	static Cont goTo(Cont cont) {
 		return new Cont() {
 			public Optional<Cont> run() throws MyException {
 				return cont.run();
@@ -110,11 +83,22 @@ abstract class Cont {
 		};
 	}
 	
-	/*static Cont performThrough(Cont[] contList, int begin, int end) {
+	static Cont performThrough(Cont[] contList, int begin, int end) {
 		return new Cont() {
 			public Optional<Cont> run() throws MyException {
-				
+				Optional<Cont> nextCont = Optional.of(contList[begin]);
+				int executedProgramId;
+				do {
+					Cont cont = nextCont.get();
+					executedProgramId = cont.contId;
+					nextCont = cont.run();
+				} while(nextCont.isPresent() && executedProgramId != end);
+				return Optional.of(Cont.pure());
 			}
-		}
-	}*/
+		};
+	}
+
+	static Cont perform(Cont[] contList, int labelId) {
+		return Cont.performThrough(contList, labelId, labelId);
+	}
 }
